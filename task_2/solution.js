b574a1273aca895abaa062d2d36cd33b441c59e2
romson <roman.guryanov@gmail.com>
2023-05-02 22:25:19 +0300

    task 2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -15,6 +15,17 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
+    
+    let shippingSum;
+
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    }
+
+    if (productsSum > 0 && productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice;
+    }
+
 
     // Конец решения задания №2.1.
 
@@ -33,7 +44,12 @@ function calcDiscount(sum, min, discount) {
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
+    
+    let discountSum;
 
+    if (productsSum >= discountMinSum) {
+        discountSum = discountPart * productsSum / 100;
+    } else {discountSum = 0}
     // Конец решения задания №2.2.
 
     return discountSum;
@@ -49,6 +65,10 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
+    
+    let totalSum;
+    totalSum = productsSum;
+    totalSum = totalSum - discountSum;
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
@@ -57,7 +77,11 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
-
+    
+    totalSum = totalSum + shippingSum;
+    freeShipping = false;
+    freeShipping = shippingSum == 0;
+    
     // Конец решения задачи №2.3.
 
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
