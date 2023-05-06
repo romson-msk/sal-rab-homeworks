426941cf7fd5509befca883654c82dcbb75601fc
romson <roman.guryanov@gmail.com>
2023-05-07 01:30:54 +0300

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,18 +34,21 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    
+    let data = {data: {client: "", order: {address, sum}, goods: []}};
+ 
+    data.data.client = name + " " + phone;
+    data.data.order.address = "ул. " + address.street + ", " + 
+                              "дом " + address.house + ", " +
+                              address.entrance + " подъезд, " +
+                              address.floor + " этаж, кв. " +
+                              address.flat;
+    data.data.order.sum = sum;
 
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {   
+        data.data.goods.push({"title": goods[i].title, "count": goods[i].count});
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
 
     let jsonData = JSON.stringify(data);
 
